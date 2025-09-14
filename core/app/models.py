435e76ddefd8eb19f65ee8ffc5e33add0484d87a
ivirare <sintervx@gmail.com>
2025-09-14 18:58:01 +0300

    Sound model added

--- core/app/models.py
+++ core/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from taggit.managers import TaggableManager
+from users.models import User
 
-# Create your models here.
+
+class Sound(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="sounds",
+    )
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to=settings.SOUNDS_DIR)
+    tags = TaggableManager()
+    likes = models.ManyToManyField(
+        User,
+        related_name="liked_sounds",
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
--- core/telepad/settings.py
+++ core/telepad/settings.py
@@ -15,6 +15,9 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost"])
 
+# -- VARIABLES --
+SOUNDS_DIR = BASE_DIR.parent + "sounds/"
+
 # -- APPLICATIONS --
 INSTALLED_APPS = [
     # Django defaults
