262176d7cc934bcd888e099b41624bf415b6c582
ivirare <sintervx@gmail.com>
2025-09-30 18:50:21 +0300

    Instantly saving sounds on creation

--- core/app/tasks/downloads.py
+++ core/app/tasks/downloads.py
@@ -5,25 +5,25 @@ from celery import shared_task
 from app.models import Sound
 from app.downloader import download_and_convert, upload_to_telegram
 from telepad.settings import MEDIA_ROOT
-from users.models import User
 
 
 @shared_task
 def download_sound(user_id: int, url: str):
     path = None
     try:
-        user = User.objects.get(pk=user_id)
-        path, title, duration = download_and_convert(url, user.id)
+        path, title, duration = download_and_convert(url, user_id)
 
         file_id = upload_to_telegram("/media/" + path, title, duration)
 
         sound = Sound.objects.create(
-            owner=user,
+            owner=user_id,
             name=title,
             file_id=file_id,
             duration=duration,
             is_private=True,
         )
+        sound.saves.add(user_id)
+
         return {
             "status": "ok",
             "sound_id": sound.id,
@@ -38,47 +38,50 @@ def download_sound(user_id: int, url: str):
 
 @shared_task
 def upload_sound(user_id: int, temp_file, filename):
-    basename, _ = os.path.splitext(filename)
-    output_file = os.path.join(MEDIA_ROOT, f"{basename}.ogg")
+    return
+#     basename, _ = os.path.splitext(filename)
+#     output_file = os.path.join(MEDIA_ROOT, f"{basename}.ogg")
 
-    command = [
-        "ffmpeg",
-        "-i",
-        temp_file,
-        "-c:a",
-        "libopus",
-        "-ar",
-        "48000",
-        "-ac",
-        "1",
-        "-b:a",
-        "48k",
-        output_file,
-    ]
+#     command = [
+#         "ffmpeg",
+#         "-i",
+#         temp_file,
+#         "-c:a",
+#         "libopus",
+#         "-ar",
+#         "48000",
+#         "-ac",
+#         "1",
+#         "-b:a",
+#         "48k",
+#         output_file,
+#     ]
 
-    try:
-        user = User.objects.get(pk=user_id)
-        subprocess.run(command, check=True, capture_output=True, text=True)
+#     try:
+#         file_id = upload_to_telegram("/media/" + output_file, title, duration)
 
-        sound = Sound.objects.create(
-            owner=user,
-            name=basename,
-            file=output_file,
-            is_private=True,
-        )
-        return {
-            "status": "ok",
-            "sound_id": sound.id,
-            "name": sound.name,
-        }
+#         subprocess.run(command, check=True, capture_output=True, text=True)
+#         sound = Sound.objects.create(
+#             owner=user_id,
+#             name=basename,
+#             file=file_id,
+#             duration=duration,
+#             is_private=True,
+#         )
 
-    except subprocess.CalledProcessError as error:
-        return {
-            "status": "failed",
-            "detail": str(error.stderr),
-        }
+#         return {
+#             "status": "ok",
+#             "sound_id": sound.id,
+#             "name": sound.name,
+#         }
 
-    finally:
-        if os.path.exists(temp_file):
-            os.remove(temp_file)
-            logging.info("Cleaned up temp file.")
+#     except subprocess.CalledProcessError as error:
+#         return {
+#             "status": "failed",
+#             "detail": str(error.stderr),
+#         }
+
+#     finally:
+#         if os.path.exists(temp_file):
+#             os.remove(temp_file)
+#             logging.info("Cleaned up temp file.")
