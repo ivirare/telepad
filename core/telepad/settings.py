ea22c26d72b9d6752417d6fb948f7c68cd250690
ivirare <sintervx@gmail.com>
2025-09-30 14:59:49 +0300

    Authtoken entry

--- core/telepad/settings.py
+++ core/telepad/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third-party
     "rest_framework",
+    "rest_framework.authtoken",
     "taggit",
     # Local apps
     "users.apps.UsersConfig",
